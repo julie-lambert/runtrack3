63cca88c24dddd1e76630567aa72c0c3b5c559c1
julie-lambert <julie.lambert@laplateforme.io>
2023-01-16 12:27:04 +0100

    job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+function afficherjourssemaines(){
+
+    let jourssemaines = ['lundi','Mardi','mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
+
+    for (let i = 0 ; jourssemaines[i]; i++){
+        console.log(jourssemaines[i]);
+    }
+}
+
+afficherjourssemaines();
