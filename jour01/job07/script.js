bbd1375a1a3011d5fbdc098ed8b33435edf0764b
julie-lambert <julie.lambert@laplateforme.io>
2023-01-17 09:22:47 +0100

    debut job 7

---
+++ jour01/job07/script.js
@@ -0,0 +1,25 @@
+const date = new Date('01-01-2023')
+
+
+function jourtravaille(date){
+
+        let jour = date.getDate();
+        let mois = date.getMonth();
+        let annee = date.getFullYear();
+        let joursemaine = date.getDay();
+        
+        let jourferie = [
+            '10-04-2023',
+            '01-05-2023',
+            '08-05-2023',
+            '18-05-2023',
+            '29-05-2023',
+            '14-07-2023',
+            '15-08-2023',
+            '01-11-2023',
+            '11-11-2023',
+            '25-12-2023'
+        ]
+
+
+}
